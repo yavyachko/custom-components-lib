625a2e7e97556233a713b61696c7425da8b421a7
yavyachko <konkursirus@gmail.com>
2025-06-26 16:20:02 +0300

    feat: add button component and its props

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,7 @@ import type { Preview } from "@storybook/react-webpack5";
 const preview: Preview = {
   parameters: {
     controls: {
+      expanded: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
