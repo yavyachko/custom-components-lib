00eb725ed139ab39f31784ee551dd5c3c356e271
yavyachko <konkursirus@gmail.com>
2025-07-04 16:40:23 +0300

    fix: change scss variables for css variables and button component props

--- src/modules/Button/Button.tsx
+++ src/modules/Button/Button.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+
 import "./Button.scss";
 
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant: "text" | "contained" | "outlined";
-  size: "small" | "medium" | "large";
+export type ButtonProps = React.ComponentProps<"button"> & {
+  variant?: "text" | "contained" | "outlined";
+  size?: "small" | "medium" | "large";
+  classes?: string;
 };
 
-export const Button: React.FC<ButtonProps> = props => {
+export const Button: React.FC<ButtonProps> = ({
+  variant = "contained",
+  size = "medium",
+  classes = "",
+  children,
+  ...props
+}) => {
   return (
     <button
       {...props}
-      className={`button button_${props.variant ?? "contained"} button_${props.size ?? "medium"}`}
+      className={`button button_${size} button_${variant} ${classes}`}
     >
-      {props.children}
+      {children}
     </button>
   );
 };
